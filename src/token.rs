2d7addfd51fe6717367349c53e593b000f43ec50
Shatyaki Dutt <shatyaki_dutt@hotmail.com>
2024-12-16 19:27:01 +0530

    Add tokens

---
+++ src/token.rs
@@ -0,0 +1,26 @@
+struct Token {
+  kind: TokenKind,
+  literal: String,
+}
+
+enum TokenKind {
+  Illegal,
+  Eof,
+
+  Ident,
+  Int,
+
+  Assign,
+  Plus,
+
+  Comma,
+  Semicolon,
+
+  Lparen,
+  Rparen,
+  Lbrace,
+  Rbrace,
+
+  Function,
+  Let,
+}
